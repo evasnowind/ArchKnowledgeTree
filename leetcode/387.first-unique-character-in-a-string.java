d78aa64e892aa514920d8c8fa25a41dbe6c187b2
Chenglong Yu <chenglong.yu@100credit.com>
2019-10-03 10:23:01 +0800

    leetcode 387 first unique character in a string

---
+++ leetcode/387.first-unique-character-in-a-string.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=387 lang=java
+ *
+ * [387] First Unique Character in a String
+ */
+
+// @lc code=start
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+    
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+        
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1) 
+                return i;
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
