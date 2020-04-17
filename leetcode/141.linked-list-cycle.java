4f4cefefc784a59c26a92e8d70715f233ff02d0e
Chenglong Yu <chenglong.yu@100credit.com>
2020-04-17 23:11:00 +0800

    leetcode 20 valid parenthese; 141 linked list cycle

---
+++ leetcode/141.linked-list-cycle.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=141 lang=java
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
--- leetcode/20.valid-parentheses.java
+++ leetcode/20.valid-parentheses.java
@@ -1,3 +1,5 @@
+import java.util.Deque;
+
 /*
  * @lc app=leetcode id=20 lang=java
  *
@@ -7,7 +9,26 @@
 // @lc code=start
 class Solution {
     public boolean isValid(String s) {
-        
+        Deque<Character> stack = new LinkedList<>();
+
+        char[] chs = s.toCharArray();
+     
+        for (int i = 0; i < chs.length; i++) {
+            char ch = chs[i];
+            if (!stack.isEmpty()) {
+                char peekCh = stack.peek();
+                if ((peekCh == '{' && ch == '}') || (peekCh == '[' && ch == ']') || (peekCh == '(' && ch == ')')) {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            } else {
+                stack.push(ch);
+            }
+            
+        }
+
+        return stack.isEmpty();
     }
 }
 // @lc code=end
