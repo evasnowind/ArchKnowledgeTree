0ccb8c2131fe6b73b02ed4f1a8a99de47a46173b
Chenglong Yu <chenglong.yu@100credit.com>
2020-06-21 20:57:57 +0800

    java AQS 源码分析、自定义锁的demo

---
+++ code/JUC/src/main/java/com/prayerlaputa/juc/part1_jol/IntegerObjectLayout.java
@@ -0,0 +1,23 @@
+package com.prayerlaputa.juc.part1_jol;
+
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author chenglong.yu@brgroup.com
+ * created on 2020/6/20
+ */
+public class IntegerObjectLayout {
+
+    public static void main(String[] args) {
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Integer intObj = new Integer(13);
+        System.out.println(ClassLayout.parseInstance(intObj).toPrintable());
+    }
+}
---
+++ code/JUC/src/main/java/com/prayerlaputa/juc/part3_aqs/prayer_version/PrayerMutex.java
@@ -0,0 +1,98 @@
+package com.prayerlaputa.juc.part3_aqs.prayer_version;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 示例来自于AQS代码注释。
+ * 官方文档
+ *
+ * @author chenglong.yu@brgroup.com
+ * created on 2020/6/21
+ */
+public class PrayerMutex implements Lock {
+
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+
+        private void readObject(ObjectInputStream s)
+                throws IOException, ClassNotFoundException {
+            s.defaultReadObject();
+            setState(0); // reset to unlocked state
+        }
+
+    }
+
+
+    private static final Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+
+    public boolean isLocked() { return sync.isHeldExclusively(); }
+
+    public boolean hasQueuedThreads() {
+        return sync.hasQueuedThreads();
+    }
+}
