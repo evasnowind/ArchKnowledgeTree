d40b69137db717fdbdb6fecb2a605414ec3bea5f
Chenglong Yu <chenglong.yu@100credit.com>
2020-06-25 11:01:38 +0800

    change mind mapping format, use xmind instead of mindmaster

---
+++ leetcode/16.3-sum-closest.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=16 lang=java
+ *
+ * [16] 3Sum Closest
+ */
+
+// @lc code=start
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        
+    }
+}
+// @lc code=end
+
