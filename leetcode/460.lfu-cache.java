31b6ec9c13bccb87aac5ad2f3921867f0791c2c0
Chenglong Yu <yuchenglong8809@163.com>
2021-02-12 11:05:13 +0800

    update leetcode solution

---
+++ leetcode/460.lfu-cache.java
@@ -0,0 +1,111 @@
+import java.util.LinkedHashSet;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=460 lang=java
+ *
+ * [460] LFU Cache
+ */
+
+// @lc code=start
+class LFUCache {
+
+    Map<Integer, Node> cache;  // 存储缓存的内容
+    Map<Integer, LinkedHashSet<Node>> freqMap; // 存储每个频次对应的双向链表
+    int size;
+    int capacity;
+    int min; // 存储当前最小频次
+
+    public LFUCache(int capacity) {
+        cache = new HashMap<> (capacity);
+        freqMap = new HashMap<>();
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        Node node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+        freqInc(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        if (capacity == 0) {
+            return;
+        }
+        Node node = cache.get(key);
+        if (node != null) {
+            node.value = value;
+            freqInc(node);
+        } else {
+            if (size == capacity) {
+                Node deadNode = removeNode();
+                cache.remove(deadNode.key);
+                size--;
+            }
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addNode(newNode);
+            size++;     
+        }
+    }
+
+    void freqInc(Node node) {
+        // 从原freq对应的链表里移除, 并更新min
+        int freq = node.freq;
+        LinkedHashSet<Node> set = freqMap.get(freq);
+        set.remove(node);
+        if (freq == min && set.size() == 0) { 
+            min = freq + 1;
+        }
+        // 加入新freq对应的链表
+        node.freq++;
+        LinkedHashSet<Node> newSet = freqMap.get(freq + 1);
+        if (newSet == null) {
+            newSet = new LinkedHashSet<>();
+            freqMap.put(freq + 1, newSet);
+        }
+        newSet.add(node);
+    }
+
+    void addNode(Node node) {
+        LinkedHashSet<Node> set = freqMap.get(1);
+        if (set == null) {
+            set = new LinkedHashSet<>();
+            freqMap.put(1, set);
+        } 
+        set.add(node); 
+        min = 1;
+    }
+
+    Node removeNode() {
+        LinkedHashSet<Node> set = freqMap.get(min);
+        Node deadNode = set.iterator().next();
+        set.remove(deadNode);
+        return deadNode;
+    }
+}
+
+class Node {
+    int key;
+    int value;
+    int freq = 1;
+
+    public Node() {}
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+// @lc code=end
+
