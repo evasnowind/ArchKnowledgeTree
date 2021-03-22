7b51feb06896660a02e488e4860c9d6ace1dc39b
Chenglong Yu <yuchenglong8809@163.com>
2021-03-22 10:34:02 +0800

    add mind map of mysql mvcc, how to design a ranking list

--- leetcode/191.number-of-1-bits.java
+++ leetcode/191.number-of-1-bits.java
@@ -6,18 +6,38 @@
 
 // @lc code=start
 public class Solution {
+	/**
+	 * 思路1：
+	 * 直接计算每一位是否为1，因此需要一个掩码mask
+	 * 来获得只有一位是1的数去&。
+	 * 
+	 * 思路2：
+	 * 利用n&(n-1)，每次可以消去最后一位1的特性，
+	 * 加速计算过程。
+	 * 
+	 * @param n
+	 * @return
+	 */
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        int mask = 1;
-    	int sum = 0;
-		for (int i = 0; i < 32; i++) {
-			if((n & mask) != 0) {
-				sum += 1;
-			}
-			mask = mask << 1;
-		}
+        // int mask = 1;
+    	// int sum = 0;
+		// for (int i = 0; i < 32; i++) {
+		// 	if((n & mask) != 0) {
+		// 		sum += 1;
+		// 	}
+		// 	mask = mask << 1;
+		// }
 		
-		return sum;
+		// return sum;
+
+		int sum = 0;
+
+        while(n != 0) {
+            sum++;
+            n = n & (n-1);
+        }
+        return sum;
     }
 }
 // @lc code=end
