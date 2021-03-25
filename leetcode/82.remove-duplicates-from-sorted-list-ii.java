add031f56d6abc2b918dbb3da8568e52047990eb
Chenglong Yu <yuchenglong8809@163.com>
2021-03-25 09:51:26 +0800

    lc 82 remove duplicates from sorted list ii; update redis mind map

---
+++ leetcode/82.remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=82 lang=java
+ *
+ * [82] Remove Duplicates from Sorted List II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+    }
+}
+// @lc code=end
+
